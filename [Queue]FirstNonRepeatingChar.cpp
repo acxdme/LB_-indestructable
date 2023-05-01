188b6a6c87b48fd252981b0384ce3b37f8e18245
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-05-01 18:25:56 +0530

    FirstNonRepeatingCharInStream.cpp
    Find the first non-repeating char in the stream.
    Algo:
    1. Initialize a `map` to keep track of the frequency of each character in the input string, and a `queue` to store the non-repeating characters.
    2. Iterate over each character in the input string. For each character, check if it has appeared before. If it has not, add it to the `queue`. If it has, remove it from the `queue` if it is in there.
    3. At each iteration, append either the character at the front of the `queue` or "#" to the output string `ans`, depending on whether the `queue` is empty or not. After all characters have been processed, return the `ans` string.

---
+++ [Queue]FirstNonRepeatingChar.cpp
@@ -0,0 +1,31 @@
+/*
+Time complexity : O(26*N)
+space complexity : O(N)
+*/		
+
+string FirstNonRepeating(string A){
+		    string ans;
+		    if(A.size() == 0 ) return ans;
+		    map<char,int> mp;
+		    queue<char> q;
+		    
+		    for(auto c : A){
+		        if(mp.find(c) == mp.end()){
+		            q.push(c);
+		        }
+		        mp[c]++;
+		        
+		        while( !q.empty() && mp[q.front()] > 1 ){
+		            q.pop();
+		        }
+		        
+		        if(q.empty()){
+		            ans += "#";
+		        }else{
+		            ans += q.front();
+		        }
+		        
+		    }
+		    
+		    return ans;
+		}
