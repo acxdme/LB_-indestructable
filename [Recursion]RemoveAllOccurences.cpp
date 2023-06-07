f0b6977a813829ce2b204c3c50d96d977ca0dcb3
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-06-07 19:55:21 +0530

    Remove all occurence of substr.
    leetcode-1910

---
+++ [Recursion]RemoveAllOccurences.cpp
@@ -0,0 +1,30 @@
+/*
+  Time complexity : O(N)
+  Space complexity : O(N)
+*/
+
+
+class Solution {
+public:
+
+    void solve(string& s, string& part){
+    //    cout<<s<<" ";
+       if(s.find(part)!= string::npos){
+           int index = s.find(part);
+             s = s.substr(0,index) + s.substr(index+part.length());
+
+            solve(s,part);
+       }
+        
+        
+    }
+    string removeOccurrences(string s, string part) {
+
+        string ans;
+        solve(s,part);
+
+
+        return s;
+        
+    }
+};
