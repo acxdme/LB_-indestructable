d1993b0b741b66287ccdbdc6770aa61c7bbc9df0
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-06-15 22:57:03 +0530

    update with more structured recursive approach

--- DP/PartitionArrayIntoMinSumDifference.cpp
+++ DP/PartitionArrayIntoMinSumDifference.cpp
@@ -16,3 +16,26 @@ void findDiff(vector<int>& nums,int index,int &n ,int subset,int p2, int &ans,in
         findDiff(nums,index+1,n,subset,p2,ans,total);
 
     }
+
+// second attempt , given a proper stucture to recursive approach
+
+    int solveRec(vector<int>& nums,int index,int &n ,int subset,int p2,int& total){
+        //  cout<<"index-"<<index<<" subset :"<<subset<<" p2 :"<<p2<<endl;
+
+        if(subset == n/2){
+            int p1 = total - p2;
+            int result = abs(p2 - p1);
+            return result;
+        }
+        if(index >= n || subset > n/2){
+            return INT_MAX;
+        }
+
+        //include
+         int include = solveMem(nums,index+1,n,subset+1,p2+nums[index],total);
+        //exlude
+        int exclude = solveMem(nums,index+1,n,subset,p2,total);
+
+        int ans = min(include,exclude);
+        return ans;
+    }
