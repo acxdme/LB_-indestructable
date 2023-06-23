3abc9dbce302c570c8954952c774b2f8629202de
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-06-23 17:41:07 +0530

    Depth first seach
    dfs method with template<typename T> .

---
+++ Graph/DFS.cpp
@@ -0,0 +1,64 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+#include<unordered_map>
+#include<list>
+
+using namespace std;
+
+template<typename T> 
+class Graph{
+public:
+    unordered_map<T,list<T>> adjList;
+    
+   void addEdge(T u ,T v ,int direction){
+        adjList[u].push_back(v);
+        if(!direction){
+            adjList[v].push_back(u);
+        }
+    }
+    
+   void printAdjList(){
+       for( auto nodes : adjList){
+           cout << nodes.first<<"-> ";
+           for(auto nbr : nodes.second){
+               cout<< nbr<<",";
+           }cout<<endl;
+       }
+   }
+   
+   void dfs(vector<int>& visited ,int src){
+       
+       cout<< src <<" ";
+       for(auto nbr : adjList[src]){
+           if(!visited[nbr]){
+               visited[nbr] = true;
+               dfs(visited,nbr);
+           }
+       }
+   }
+    
+};
+int main() {
+   
+    Graph<int> g;
+    g.addEdge(0,1,1);
+    g.addEdge(1,2,1);
+    g.addEdge(1,4,1);
+    g.addEdge(2,4,1);
+    g.addEdge(2,3,1);
+    
+    g.printAdjList();
+    int nodes = 5;
+    vector<int> visited(nodes , 0);
+    
+    for(int i = 0 ;i < nodes ; i++){
+        if(!visited[i]){
+            g.dfs(visited,i);
+        }
+    }
+    
+    
+
+    return 0;
+}
