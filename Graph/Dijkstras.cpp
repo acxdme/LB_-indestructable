db0e96b2807f24e61508c5ce1ed6736b30fdc62a
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-06-28 23:39:50 +0530

    Update Dijkstras.cpp

--- Graph/Dijkstras.cpp
+++ Graph/Dijkstras.cpp
@@ -1,1 +1,6 @@
 //Greedy algorithm
+
+/*
+
+  algorithm : find the node with minimum distance , staring with source node,keep updating the distance array , and maintain a clean set of {distance, node} values.
+  */
