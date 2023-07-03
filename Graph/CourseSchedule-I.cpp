1bf922657765155d0a4f27eaa16f53b58f532354
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-07-03 22:38:51 +0530

    updated with bfs approach

--- Graph/CourseSchedule-I.cpp
+++ Graph/CourseSchedule-I.cpp
@@ -44,3 +44,60 @@ public:
 
     }
 };
+
+//working solution
+// Kahn's algorithm (BFS approch)
+class Solution {
+public:
+    bool findTopoOrderUsingKahn(int nodes ,unordered_map<int,list<int>>& adjList){
+        queue<int> q;
+        vector<int> indegree(nodes,0);
+        for(auto it : adjList){
+            for(auto nbr : it.second){
+                indegree[nbr]++;
+            }
+        }
+        for(int i =0 ;i<nodes;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        vector<int> topoOrder;
+        while(!q.empty()){
+            int frontNode = q.front();
+            topoOrder.push_back(frontNode);
+            q.pop();
+            for(auto nbr : adjList[frontNode]){
+                indegree[nbr]--;
+                if(indegree[nbr] == 0){
+                    q.push(nbr);
+                }
+            }
+        }
+
+        if(topoOrder.size() == nodes){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        //create adjList from prerequisites.
+        // apply kahns algorithm.
+        // validate topo ordering.
+        unordered_map<int,list<int>> adjList;
+        for(auto it : prerequisites){
+            int u = it[0];
+            int v = it[1];
+            adjList[v].push_back(u);
+        }
+
+        bool result=  findTopoOrderUsingKahn(numCourses,adjList);
+
+
+        return result;
+
+    }
+};
