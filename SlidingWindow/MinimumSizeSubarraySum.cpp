0ef8844f894216b8689bc0e62fd1e207ac271664
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-07-29 23:00:06 +0530

    Minimum size subarray sum

---
+++ SlidingWindow/MinimumSizeSubarraySum.cpp
@@ -0,0 +1,39 @@
+//leetcode-209
+
+/** algo :  a)start with window of size 1 from left
+             b)expand upto target
+             c)minimize for target
+**/            
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+
+        int start = 0;
+        int end = 0;
+        int size = nums.size();
+        int minLength = INT_MAX;
+        int sum  = 0;
+
+        while( end < size){
+            sum += nums[end];
+
+            if(sum >= target){
+
+                while(start <= end && sum >= target){
+                    sum -= nums[start];
+                    start++;
+                }
+
+                minLength = min(minLength , end - start +2);
+            }
+            end++;
+        }
+
+        if(minLength == INT_MAX){
+            return 0;
+        }
+
+        return minLength;
+    }
+};
