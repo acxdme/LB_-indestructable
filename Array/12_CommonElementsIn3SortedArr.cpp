627080d0640c53473bd5ca0968835d385ee3dfe0
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-08-13 11:57:30 +0530

    optimal approach added.

--- Array/12_CommonElementsIn3SortedArr.cpp
+++ Array/12_CommonElementsIn3SortedArr.cpp
@@ -28,3 +28,28 @@
         
         return ans;
     }
+
+
+// approach-2  , TC:O(N) , S.C : O(1) , take advantage of the fact that the arrays are sorted.
+
+            vector<int> ans;
+            int p1 = 0 , p2 = 0 ,p3 = 0;
+            
+            for(;p1 < a ;p1++){
+                if(p1 !=0 &&  A[p1-1] == A[p1]){
+                    continue;
+                }
+                int numA = A[p1];
+                while(p2 < b && B[p2] < numA){
+                    p2++;
+                }
+                while(p3 < c && C[p3] < numA){
+                    p3++;
+                }
+                if( numA == B[p2] && numA == C[p3]){
+                    ans.push_back(numA);
+                }
+                
+            }
+            
+            return ans;
