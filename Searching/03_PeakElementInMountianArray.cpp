070e21ed844d8172ad92ae692e9be7c0c82fbae8
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-08-23 23:57:58 +0530

    peak element in mountain array

--- Searching/03_PeakElementInMountianArray.cpp
+++ Searching/03_PeakElementInMountianArray.cpp
@@ -1,1 +1,24 @@
-// lc -
+// lc - 852
+// Time complexity : O(log N)
+// Space complexity : O(1)
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        int start = 0 ;
+        int end = n-1;
+        int mid = start + (end-start)/2;
+
+        while(start < end){
+            if(arr[mid] < arr[mid+1]){
+                start = mid +1;
+            }else{
+                end = mid;
+            }
+            mid = start + (end-start)/2;
+        }
+        
+        return start;
+    }
+};
