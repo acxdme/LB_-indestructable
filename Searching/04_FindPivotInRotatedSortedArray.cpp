4316a04bd37a14729e8934278fa1cf1764ce7159
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-08-24 22:46:14 +0530

    Create 04_FindPivotInRotatedSortedArray.cpp

---
+++ Searching/04_FindPivotInRotatedSortedArray.cpp
@@ -0,0 +1,3 @@
+// lc -
+// alag alag flavours hai iske , find min ,find max , find pivot
+// not able to make the conditions , had to watch solution , after watching find pivot solution need to run 5 test cases to absorb.
