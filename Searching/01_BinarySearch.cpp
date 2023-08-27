a9e49df6201e1862d20b5cf7d70c3094ad98044d
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-08-27 23:27:38 +0530

    binary search approach

--- Searching/01_BinarySearch.cpp
+++ Searching/01_BinarySearch.cpp
@@ -1,1 +1,21 @@
 // tc   o(log n)
+
+int main (){
+  int nums[] = { 1,2 , 6,7 ,9 ,19};
+  int start = 0 ;
+  int end = nums.size() -1;
+  int mid = start + (end -start)/2;
+  while(start <= end){
+    if(nums[mid] == target){
+      return mid;
+    }
+    else if (nums[mid] > target){
+      start  = mid +1;
+    }
+    else{
+      // nums[mid] < target
+      end = mid -1;
+  }
+
+    return -1;
+}
