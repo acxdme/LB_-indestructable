93d2668b8f79b09483698339323886d8d76f0756
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-08-30 22:47:47 +0530

    Direct approach added.

--- Searching/06_SearchInRotatedSortedArray.cpp
+++ Searching/06_SearchInRotatedSortedArray.cpp
@@ -1,3 +1,42 @@
 // Method-1
 // a)Find the pivot element (max wala)
 // b) apply binary search on the subarray for which the target lies in the range of subarray.
+
+
+//method-2
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int start = 0 ;
+        int end = n -1;
+        int mid = start + (end-start)/2;
+        int ans = -1;
+        while(start <= end){
+            if(target == nums[mid]){
+                ans = mid;
+                break;
+            }
+            if(nums[mid] >= nums[start]){
+                if( target >= nums[start] && target < nums[mid])
+                  end = mid -1;
+                else{
+                    start = mid +1;
+                }
+            }
+            else if(nums[end] >= nums[mid]){
+                if( target > nums[mid] && target <= nums[end]){
+                    start = mid + 1;
+                }
+                else{
+                    end = mid -1;
+                }
+            }
+            mid = start + (end-start)/2;
+        }
+
+        return ans;
+        
+    }
+};
