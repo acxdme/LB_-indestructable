be51617781cf4247f8abe46d1366d46275a3f673
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-09-24 22:32:10 +0530

    Defranging An IP Address
    . -> [.]

---
+++ CharArrays&Stings/02_DefangingAnIpAddress.cpp
@@ -0,0 +1,22 @@
+// tc : O(N)
+// sc : O(1)
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+
+        int i = 0 ;
+        int size = address.length();
+
+        for(int i = 0 ; i < size ; i++){
+            if(address[i] == '.' ){
+                address.replace(i,1,"[.]");
+                size = address.length();
+                i++;
+            }
+        }
+
+        return address;
+        
+    }
+};
