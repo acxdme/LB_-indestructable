e14fc7927c983e0e45d55c3b08dc6086e6c881a5
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-09-24 22:33:53 +0530

    optimized approach updated.

--- CharArrays&Stings/02_DefangingAnIpAddress.cpp
+++ CharArrays&Stings/02_DefangingAnIpAddress.cpp
@@ -21,3 +21,25 @@ public:
         
     }
 };
+
+// more optimized approach with i+2 skips if . is found
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+
+        int i = 0 ;
+        int size = address.length();
+
+        for(int i = 0 ; i < size ; i++){
+            if(address[i] == '.' ){
+                address.replace(i,1,"[.]");
+                size = address.length();
+                i+=2;
+            }
+        }
+
+        return address;
+        
+    }
+};
