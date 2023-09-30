c84c136e54ddf098e987a6efcab8cfd662d49664
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-09-30 13:14:52 +0530

    debug 04
    This code tries to implement Bubble Sort.

---
+++ Debug/Searcing&Sorting/04.cpp
@@ -0,0 +1,27 @@
+// debug -04
+//debugging :  i  < n - 1 was the correction.
+
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {1,6,2,8,4 };
+    int n = 5;
+    bubbleSort(arr, n);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }cout << endl;
+
+    return 0;
+}
