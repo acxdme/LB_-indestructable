bbc80ebc5e533100d421b73bdbf4292e6b7378c8
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-10-01 18:30:43 +0530

    sorting and Array hashtable approaches added

--- CharArrays&Stings/06_ValidAnagrams.cpp
+++ CharArrays&Stings/06_ValidAnagrams.cpp
@@ -1,8 +1,24 @@
 // tc : O(N)
 // sc : O(1) , since considering only 26 small englist alphabets.
-// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
-// typically using all the original letters exactly once.
 // lc-242
+/**
+     An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+     typically using all the original letters exactly once.
+**/
+
+// approach -1 (sorting)
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
+
+
+// approach-2 (hashmap)
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -22,6 +38,34 @@ public:
             if(arr[i] != 0) return false; // check non-null values
         }cout<<endl;
 
+        return true;
+    }
+};
+
+// approach-3 (hashTable using array)
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int count[26] = {0};
+        
+        // Count the frequency of characters in string s
+        for (char x : s) {
+            count[x - 'a']++;
+        }
+        
+        // Decrement the frequency of characters in string t
+        for (char x : t) {
+            count[x - 'a']--;
+        }
+        
+        // Check if any character has non-zero frequency
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        
         return true;
     }
 };
