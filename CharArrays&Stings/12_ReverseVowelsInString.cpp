dae2e4d9b40ca33c345841ed96e235f9212dcca5
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-10-02 12:53:12 +0530

    improved isVowel function

--- CharArrays&Stings/12_ReverseVowelsInString.cpp
+++ CharArrays&Stings/12_ReverseVowelsInString.cpp
@@ -32,3 +32,36 @@ public:
         return s;
     }
 };
+
+// approach -2 ( bit more optimized)
+class Solution {
+public:
+    bool isVowel(char x){
+       
+        if(x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u'
+          || x == 'A' || x == 'E' || x == 'I' || x == 'O' || x == 'U'){
+            return true;
+        }
+
+        return false;
+    }
+    string reverseVowels(string s) {
+        int start = 0;
+        int end = s.length() -1;
+        while(start <= end){
+            if(isVowel(s[start]) && isVowel(s[end])){
+                swap(s[start],s[end]);
+                start++;
+                end--;
+            }
+            else if(!isVowel(s[start])){
+                start++;
+            }
+            else{
+                end--;
+            }
+        }
+
+        return s;
+    }
+};
