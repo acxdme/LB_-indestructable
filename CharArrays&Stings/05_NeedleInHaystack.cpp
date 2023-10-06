f0d6b084a936f0e222361104516a74bc034a77a9
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-10-06 22:44:22 +0530

    finding first occurence in string

--- CharArrays&Stings/05_NeedleInHaystack.cpp
+++ CharArrays&Stings/05_NeedleInHaystack.cpp
@@ -2,9 +2,9 @@
 // sc : O(1)
 // lc-28
 
+// method-1
     int strStr(string haystack, string needle) {
         int res =  -1;
-        int n = haystack.size();
          auto it =  haystack.find(needle);
          if(it != string::npos){
              res= it;
@@ -13,3 +13,21 @@
 
         return res;
     }
+
+
+// method-2
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int result = -1;
+        if(needle.size() > haystack.size()) return result;
+        for(int i = 0 ; i <= haystack.size() - needle.size(); i++){
+            if(haystack.substr(i,needle.size()) == needle){
+                result = i;
+                break;
+            }
+        }
+
+        return result;
+    }
+};
