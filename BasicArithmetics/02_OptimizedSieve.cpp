36d4e23fbdc70bd072dd97c6ba3a0a00ba2d21bb
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-10-24 22:03:55 +0530

    count primes optimized

--- BasicArithmetics/02_OptimizedSieve.cpp
+++ BasicArithmetics/02_OptimizedSieve.cpp
@@ -6,14 +6,17 @@ public:
         // sieve of eratothesis
        int res = 0;
        vector<bool> sieve(n,true);
-       for(int i = 2 ; i*i< n ;i++){ // optimization - 2
+       for(int i = 2 ; i*i <= n ;i++){ // optimization - 2
            if(sieve[i]){
            int f = i * i ; // optimization - 1
             while( f < n){
                 sieve[f] = false;
-                f += i
+                f += i;
             }
            }
+       }
+       for ( int i =2 ; i < n ;i++){
+           if(sieve[i]) res ++;
        }
        return res;
     }
