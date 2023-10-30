0f6eb89d2eff7842b27a20208899fba9a88acced
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-10-30 22:28:32 +0530

    Create 05_DeferencingNullPointer.cpp

---
+++ Pointers/05_DeferencingNullPointer.cpp
@@ -0,0 +1,5 @@
+/** Dereferencing a null pointer is a common programming error.
+When a null pointer is accessed, it leads to a segmentation fault or access violation, causing the program to crash.
+  To avoid this, always check if a pointer is null before dereferencing it. 
+Using conditional statements or pointer validation techniques can prevent null pointer dereferencing and enhance program stability and reliability.
+**/
