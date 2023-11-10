3850280d68362e3f2801d95ded2dcf425e30ae1a
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-11-10 21:22:30 +0530

    Print Subsequences

---
+++ Recursion/03_PrintSubseqences.cpp
@@ -0,0 +1,35 @@
+// tc : O(2^N)
+// sc : O(log N)
+
+#include <iostream>
+using namespace std;
+void util(string &name , int i , int &n ,string ans){
+  if(i >= n ){
+    cout << ans<<",";
+    return;
+  }
+  
+  // exclude
+  util(name,i+1,n ,ans);
+
+  //include
+  util(name,i+1,n,ans + name[i]);
+
+
+}
+
+void printSubsequences(string &name){
+  int i = 0 ;
+  int n = name.size();
+  string ans;
+  util(name,i,n,ans);
+  cout << endl;
+}
+
+
+int main() {
+   
+  string name = "ab";
+  printSubsequences(name);
+  return 0;
+}
