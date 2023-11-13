bb3964775afb784349c506bc67d28f2b4582931d
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-11-13 23:40:09 +0530

    Update 02_CountingStairs.cpp

--- Recursion/02_CountingStairs.cpp
+++ Recursion/02_CountingStairs.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n == 1)
+        if(n == 1) // base case
          return 1;
-        if( n == 2)
+        if( n == 2) // base case
         return 2;
 
         return climbStairs(n-1) + climbStairs(n-2);
