f29f4571a9552af9ccc811dceea809c893348648
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-11-14 22:01:12 +0530

    include exclude order change

--- Recursion/03_PrintSubseqences.cpp
+++ Recursion/03_PrintSubseqences.cpp
@@ -33,3 +33,47 @@ int main() {
   printSubsequences(name);
   return 0;
 }
+
+
+//----------------------------------------------------------------
+// is there any change if the sequence of include exlcude is changed  -> I could not observe any change.
+
+
+// tc : O(2^N)
+// sc : O(log N)
+
+#include <iostream>
+using namespace std;
+void util(string &name , int i , int &n ,string ans){
+  if(i >= n ){
+    cout << ans<<",";
+    return;
+  }
+  
+    //include
+  util(name,i+1,n,ans + name[i]);
+
+  // exclude
+  util(name,i+1,n ,ans);
+
+
+
+
+}
+
+void printSubsequences(string &name){
+  int i = 0 ;
+  int n = name.size();
+  string ans;
+  
+  util(name,i,n,ans);
+  cout << endl;
+}
+
+
+int main() {
+   
+  string name = "abc";
+  printSubsequences(name);
+  return 0;
+}
