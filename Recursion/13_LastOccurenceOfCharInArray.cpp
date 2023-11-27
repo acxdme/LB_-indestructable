b243d3f694be4d43ac27bd2e119aa62958953f9e
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2023-11-27 22:21:48 +0530

    last occurence of char in array

--- Recursion/13_LastOccurenceOfCharInArray.cpp
+++ Recursion/13_LastOccurenceOfCharInArray.cpp
@@ -14,3 +14,28 @@
    
    
 }
+
+// recursive approch
+// time-complexity : O(N)
+// space-complexity : O(N) , considering recursion stack
+
+
+    int isCharFound(string &s , char &p , int i){
+        if(i < 0) return -1;
+        
+        if(s[i] == p) return i;
+        
+        int ans = isCharFound(s,p,i-1);
+        
+        return ans;
+    }
+    
+    int LastIndex(string s, char p){
+       int ans = -1;
+       int n = s.size();
+       ans = isCharFound(s,p,n-1);
+       
+       return ans;
+       
+       
+    }
