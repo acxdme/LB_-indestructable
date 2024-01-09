a08c478fc3cb241d5c5c97f9ae5c0904c024f0e7
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-01-09 22:52:32 +0530

    easy spot.

--- Debug/Recursion/02.cpp
+++ Debug/Recursion/02.cpp
@@ -1,1 +1,39 @@
 //N-Queens
+
+    bool isSafePlace(int n, vector<string>& nQueens, int row, int col){
+        for(int i=0; i<n; i++){ // Necessary Check to check if queen is safe to place or not.
+            if(nQueens[i][col] == 'Q'){
+                return false;
+            }
+        }
+        for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--){
+            if(nQueens[i][j] == 'Q'){
+                return false;
+            }
+        }
+        for(int i=row-1, j=col+1; i>=0 && j<n; i--, j++){
+            if(nQueens[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    void solveNQueens(int n, vector<vector<string>>& output, vector<string>& nQueens, int row){
+        if(row == n){
+            output.push_back(nQueens);
+            return;
+        }
+        for(int col=0; col<n; col++){
+            if(isSafePlace(n, nQueens, row, col)){
+                nQueens[row][col] = 'Q';
+                solveNQueens(n, output, nQueens, row+1);
+                nQueens[row][col] = '.'; // Fix: Revert back if solution is not found
+            }
+        }
+    }
+   vector<vector<string>> solve(int n) {
+        vector<vector<string>> output;
+        vector<string> nQueens(n , string(n, '.'));
+        solveNQueens(n, output, nQueens, 0);
+        return output;
+    }
