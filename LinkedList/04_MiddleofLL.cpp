7314379351599b9b1c709db07828639355b8d21c
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-01-13 13:52:43 +0530

    MIddle of Linked list

---
+++ LinkedList/04_MiddleofLL.cpp
@@ -0,0 +1,22 @@
+// slow & fast pointer approach 
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL){
+
+            fast = fast->next;
+            if(fast != NULL){
+                fast = fast->next;
+                slow = slow->next;
+            }
+        }
+
+        return slow;
+        
+    }
+};
