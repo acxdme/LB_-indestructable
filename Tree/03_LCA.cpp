8717a32c1c3bf116a77a00af8978ec688d2fb926
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-01-15 14:54:23 +0530

    comments added

--- Tree/03_LCA.cpp
+++ Tree/03_LCA.cpp
@@ -2,7 +2,7 @@
 // tc : O(N)
 // sc : O(log N)
 
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         //base-case
         if(root == NULL){
             return NULL;
@@ -16,18 +16,24 @@
             return q;
         }
 
+        // left ka answer
         TreeNode* leftAnswer = lowestCommonAncestor(root->left,p,q);
+        // right ka answer
         TreeNode* rightAnswer = lowestCommonAncestor(root->right,p,q);
 
+        // right se answer aaya , rightAnswer hi return kardo (p,q right mei hai)
         if(leftAnswer == NULL && rightAnswer != NULL){
             return rightAnswer;
         }
+        // left se answer aaya , leftAnswer hi return kardo
         else if(rightAnswer == NULL && leftAnswer != NULL){
             return leftAnswer;
         }
+        // left and right dono se answer nahi mila , return NULL
         else if(rightAnswer == NULL && leftAnswer == NULL){
             return NULL;
         }
+        // left se answer aaya and right se bhi answer aaya , ab root kardo return
         else{
             return root;
         }
