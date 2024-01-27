c3cbe89a359460da223baa99d933db92eaa10213
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-01-27 21:35:36 +0530

    03
    Print second largest element in a bst.

---
+++ Debug/BST/03.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+  public:
+  int key;
+  Node* left;
+  Node* right;
+
+  Node(int data){
+    this->key = data;
+    this->left = NULL;
+    this->right = NULL;
+  }
+};
+
+void secondLargestUtil(Node *root, int &c) {
+    if (root == NULL)
+        return;
+    secondLargestUtil(root->right, c);
+    c++; // increment on coming back from left recursive call
+    if (c == 2) {
+        c = 3;  // flag something appropriate to make logic correct.
+        cout << root->key << endl; 
+        return;
+    }
+    secondLargestUtil(root->left, c);
+}
+
+void secondLargest(Node *root) {
+    int c = 0;
+    secondLargestUtil(root, c);
+}
+
+int main() {
+
+  Node* root = new Node(50);
+  Node* first = new Node(40);
+  Node* second = new Node(60);
+  Node* third = new Node(70);
+  Node* fourth = new Node(64);
+
+  root->left = first;
+  // root->right = second;
+  // root->right->right = third;
+  // root->right->right->left = fourth;
+
+  secondLargest(root);
+
+  return 0;
+}
