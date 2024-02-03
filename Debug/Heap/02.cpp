62952526225743605465236916c3ce430972dd1b
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-02-03 22:28:52 +0530

    02

---
+++ Debug/Heap/02.cpp
@@ -0,0 +1,17 @@
+
+// Constructor to initialize data
+// members and creating mean heap
+// MinHeap::MinHeap(int size, int input[])
+// Assume this working fine working fine
+
+void firstKthElement(int arr[], int size, int k) {
+    MinHeap* m = new MinHeap(k, arr);
+    for (int i = k; i < size; i++) {
+        if (arr[0] > arr[i])
+            continue;
+        else {
+            arr[0] = arr[i];
+            m->heapify(0);
+        }
+    }
+}
