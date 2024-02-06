85f59e43250b4b93a6f1d89bff95672ab3e82b11
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-02-06 23:26:58 +0530

    03

---
+++ Debug/03.cpp
@@ -0,0 +1,20 @@
+// reverseStack , fun approach
+
+void reverseStack(stack<int> &input, stack<int> &extra) {
+	if(input.empty()){ // base-case
+        return;
+    }
+    int x = input.top();
+    input.pop();
+    reverseStack(input,extra);
+    for(int i=0; input.empty()==false; i++){ // condn
+        extra.push(input.top());
+        input.pop();
+    }
+    input.push(x);
+    for(int i=0; extra.empty()==false; i++){ // condn
+        input.push(extra.top());
+        extra.pop();
+    }
+    return;
+}
