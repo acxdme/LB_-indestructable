c326d1ef8c408980d2ec4638cdc7b7154f373cec
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-02-12 20:51:40 +0530

    Find redundant bracket or not.

---
+++ Stack/06_FindRedundantBracket.cpp
@@ -0,0 +1,43 @@
+// gfg - https://www.geeksforgeeks.org/problems/expression-contains-redundant-bracket-or-not/1
+// tc : 
+// sc : 
+
+class Solution {
+  public:
+    int checkRedundancy(string s) {
+        
+        bool flag = false;
+        int n = s.size();
+        stack<char> st;
+        
+        for(int i = 0 ; i < n ; i++){
+            if(s[i] == '('){
+                st.push(s[i]);
+            }
+            else if(s[i] == '+'  || s[i] == '-' || s[i] == '*' || s[i] == '/'){
+                st.push(s[i]);
+            }
+            else if( s[i] == ')'){
+                
+                while(!st.empty() && st.top() != '('){
+                    flag = true;
+                    st.pop();
+                    
+                }
+                if(st.top() == '(' && !flag){ // redundant bracket found
+                    return 1;
+                }else{
+                    st.pop(); // pop the correct opening bracket
+                    flag = false;
+                }
+            }
+            else{
+                continue;
+            }
+                
+        }
+        
+        return 0;
+       
+    }
+};
