6fa972d0109cba5ab064b9ca60ce25529e97b2cc
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-02-13 21:46:51 +0530

    official answer added.

--- Debug/Maps&Tries/03.cpp
+++ Debug/Maps&Tries/03.cpp
@@ -12,3 +12,21 @@ int main() {
 
     return 0;
 }
+
+// official answer
+
+#include <iostream>
+#include <map>
+
+int main() {
+    std::map<int, std::string> myMap;
+    myMap[1] = "Apple";
+    myMap[2] = "Banana";
+    myMap[3] = "Orange";
+
+    int key = 4;
+    if(myMap.find(key) != myMap.end())
+    std::cout << "Value for key " << key << ": " << myMap.at(key) << std::endl;
+
+    return 0;
+}
