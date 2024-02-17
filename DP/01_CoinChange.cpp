0b5065703687783e73de0250efaa623f268e0aa7
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-02-17 12:04:07 +0530

    taulation approach updated.

--- DP/01_CoinChange.cpp
+++ DP/01_CoinChange.cpp
@@ -1,3 +1,7 @@
+// lc -322
+// Recursion Logic : 0 -> amount.
+// resolved tle with tabulation approach.
+
 class Solution {
 public:
 
@@ -34,15 +38,41 @@ public:
 
         long long  int mini = INT_MAX;
         for(int i = 0 ; i< coins.size() ;i++){
-           long long int sum = curr + coins[i];
-           long long int ans = solveMem(coins,sum,amount,dp);
+
+           long long int ans = solveMem(coins,curr + coins[i],amount,dp);
            if(ans != INT_MAX){
              mini = min(mini, 1 + ans);
-             dp[curr] = mini;
            }
+             dp[curr] = mini;
         }
 
         return dp[curr];
+    }
+
+    // solve tab
+    long long int solveTab(vector<int>& coins , int amount){
+        vector<int> dp(amount+1 , INT_MAX);
+        // base-case
+        dp[amount] = 0;
+
+
+        for(int total = amount - 1; total >=0 ; total--){
+            long long  int mini = INT_MAX;
+            for(int i = 0 ; i< coins.size() ;i++){
+            long long int ans = INT_MAX;
+                if(coins[i] <= amount &&  total + coins[i] <= amount){
+                     ans = dp[total + coins[i]];
+                }
+                if(ans != INT_MAX){
+                    mini = min(mini, 1 + ans);
+                }
+            }
+                dp[total] = mini;
+        }
+
+        return dp[0];
+
+
     }
 
     int coinChange(vector<int>& coins, int amount) {
@@ -57,11 +87,17 @@ public:
 
         // return ans;
 
-
         // solveMem
+        // long long int curr = 0;
+        // vector<long long int> dp(amount+1 , INT_MAX);
+        // long long int ans =  solveMem(coins,curr,amount,dp);
+
+        // if(ans == INT_MAX){
+        //     return -1;
+        // }
+
         long long int curr = 0;
-        vector<long long int> dp(amount+1 , INT_MAX);
-        long long int ans =  solveMem(coins,curr,amount,dp);
+        long long int ans =  solveTab(coins,amount);
 
         if(ans == INT_MAX){
             return -1;
@@ -69,7 +105,5 @@ public:
 
         return ans;
 
-
-        
     }
 };
