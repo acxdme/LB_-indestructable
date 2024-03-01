88e5044655e85da3268bd74052b4ddc05a7d3d87
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-03-01 22:25:13 +0530

    01

---
+++ Debug/Graph/01.cpp
@@ -0,0 +1,28 @@
+// BFS , Disconnected Graph
+
+class Solution {
+  public:
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        vector<int> output;
+        vector<bool> visited(V, false);
+        for (int i = 0; i < V; ++i) { // fix : missing loop for all vertices , ensuring the disconnected component scenario.
+            if (!visited[i]) {
+                queue<int> q;
+                visited[i] = true;
+                q.push(i);
+                while (!q.empty()) {
+                    int g_node = q.front();
+                    q.pop();
+                    output.push_back(g_node);
+                    for (auto it : adj[g_node]) {
+                        if (!visited[it]) {
+                            visited[it] = true;
+                            q.push(it);
+                        }
+                    }
+                }
+            }
+        }
+        return output;
+    }
+};
