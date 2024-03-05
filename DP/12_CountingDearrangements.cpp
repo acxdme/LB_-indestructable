5d90e2b2fe59174281e2060db5b3d4becdc4476c
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-03-05 22:18:20 +0530

    CountingDearrangements

---
+++ DP/12_CountingDearrangements.cpp
@@ -0,0 +1,7 @@
+// Counting Dearrangements
+/*
+A Derangement is a permutation of ‘N’ elements, such that no element appears in its original position.
+For example, an instance of derangement of {0, 1, 2, 3} is {2, 3, 1, 0},
+because 2 present at index 0 is not at its initial position which is 2 and similarly for other elements of the sequence.
+Given a number ‘N’, find the total number of derangements possible of a set of 'N’ elements.
+*/
