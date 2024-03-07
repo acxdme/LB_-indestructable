a3829a39faec605a74eed74fe217dd07d42b114c
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-03-07 22:57:33 +0530

    dijkstra's working

--- Graph/02_Dijktra'sApplication.cpp
+++ Graph/02_Dijktra'sApplication.cpp
@@ -45,3 +45,52 @@
         
         return distance;
     }
+
+
+// {node,weight} confusion , lot of logical mistake , set removal set totally not in sync/logic weak
+// after hints : 
+
+ void findShortestPath(int &V , vector<vector<int>> adj[] , int &src , vector<int> &distance,set<pair<int,int>> &st){
+        
+        distance[src] = 0;
+        
+        st.insert({0,src});
+        
+        while(!st.empty()){
+            
+            auto it = *st.begin();
+            int currD = it.first;
+            int node = it.second;
+            
+            st.erase(st.begin());
+            
+            for(auto nbr : adj[node]){
+                if(distance[node] != INT_MAX  && currD + nbr[1] < distance[nbr[0]]){
+                    
+                    if(st.find({distance[nbr[0]],nbr[0]}) != st.end()){
+                        auto pos = st.find({distance[nbr[0]],nbr[0]});
+                        st.erase(pos);
+                    }
+                    distance[nbr[0]] =  currD + nbr[1];
+                    st.insert({distance[nbr[0]],nbr[0]});
+                }
+            }
+        }
+    }
+    
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        // Code here
+        
+        //distance array
+        vector<int> distance(V,INT_MAX); 
+        
+        
+        //set<weight,node>
+        set<pair<int,int>> st;
+        
+        findShortestPath(V,adj,S,distance,st);
+        
+        
+        return distance;
+    }
