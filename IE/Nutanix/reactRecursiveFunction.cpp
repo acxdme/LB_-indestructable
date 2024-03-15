c410dcc419eae58a73446a6f04808531c5f71080
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-03-15 23:13:19 +0530

    recursive function

---
+++ IE/Nutanix/reactRecursiveFunction.cpp
@@ -0,0 +1,77 @@
+Response = [
+
+ {
+
+  topic: Entertainment,
+
+  subTopics: [{
+
+   topic: ‘…’,
+
+   subTopics: [],
+
+   newsList: []
+
+  }, {}, …],
+
+  newsList: [{
+
+   title: ‘header’,
+
+   description: ‘details’
+
+  }, {}, …]
+
+ }
+
+]
+
+
+— Entertainment
+
+ — Movie
+
+  — Heading
+
+   — Description
+
+  — Heading
+
+   — Description
+
+ — Music
+
+  — Bollywood
+
+   — Heading
+
+    — Description
+
+  — Indie
+
+   — Heading
+
+    — Description
+
+  — Heading
+
+   — Description
+
+— Sports
+
+ — Cricket
+
+  — Heading
+
+   — Description
+
+ — Heading
+
+  — Description
+
+— Politics
+
+ — Heading
+
+  — Description
+     
