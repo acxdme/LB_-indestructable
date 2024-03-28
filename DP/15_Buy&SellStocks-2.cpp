af9d2b6d6f96a6b84b6c9aa2b3d8598f0107768b
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-03-28 21:44:18 +0530

    editorial seen

--- DP/15_Buy&SellStocks-2.cpp
+++ DP/15_Buy&SellStocks-2.cpp
@@ -1,3 +1,4 @@
 // lc -122
 // my intial thought , buy , sell and pass . Buy and Sell should be conditional(flag based);
+//update : initial thought was to the mark , use flag for buy boolean and manipute along with skip option.
 
