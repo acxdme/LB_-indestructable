3e76d5b6146542f79c63f4de85684d598b7d8b97
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-06-06 20:01:13 +0530

    Flip Equivalent Trees.

---
+++ Tree/12_FlipEquivalentBinaryTree.cpp
@@ -0,0 +1,30 @@
+TC : O(N)
+SC : O(log(N))
+lc - 951 : https://leetcode.com/problems/flip-equivalent-binary-trees/
+
+class Solution {
+public:
+
+    bool checkEquiv(TreeNode* r1 , TreeNode* r2){
+        if(r1 == NULL & r2 != NULL)
+        return false;
+        if(r1 != NULL & r2 == NULL)
+        return false;
+        if(r1 == NULL & r2 == NULL)
+        return true;
+        if(r1->val != r2->val){
+            return false;
+        }
+        // check if already equal or can be flipped to make it equal
+        return checkEquiv(r1->left,r2->left) && checkEquiv(r1->right,r2->right) || 
+        checkEquiv(r1->left,r2->right) && checkEquiv(r1->right,r2->left);
+
+        
+    }
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+
+        bool ans = checkEquiv(root1, root2);
+        return ans;
+        
+    }
+};
