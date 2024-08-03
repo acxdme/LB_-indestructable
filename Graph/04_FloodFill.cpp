cdf19cfd37d8f765268baed4c090f9043b2d8763
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-08-03 23:58:36 +0530

    Visited approach

--- Graph/04_FloodFill.cpp
+++ Graph/04_FloodFill.cpp
@@ -44,3 +44,51 @@ public:
         
     }
 };
+
+-------------------------------------------------------------------------------------------------------------
+    class Solution {
+public:
+    bool isValid(int x, int y, vector<vector<int>>& image, int originalColor) {
+        if (x < 0 || x >= image.size())
+            return false;
+        if (y < 0 || y >= image[0].size())
+            return false;
+        if (image[x][y] != originalColor)
+            return false;
+
+        return true;
+    }
+
+    void dfs(vector<vector<int>>& image, int sr, int sc,
+             vector<vector<int>>& visited, int originalColor, int newColor) {
+        visited[sr][sc] = 1;
+        image[sr][sc] = newColor;
+
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = {0, 1, 0, -1};
+
+        for (int i = 0; i < 4; i++) {
+            int x = sr + dx[i];
+            int y = sc + dy[i];
+
+            if (isValid(x, y, image, originalColor)) {
+                if (!visited[x][y]) {
+                    dfs(image, x, y, visited, originalColor, newColor);
+                }
+            }
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
+                                  int color) {
+
+        int rows = image.size();
+        int cols = image[0].size();
+        int originalColor = image[sr][sc];
+        int newColor = color;
+        vector<vector<int>> visited(rows, vector<int>(cols, 0));
+
+        dfs(image, sr, sc, visited, originalColor, newColor);
+
+        return image;
+    }
+};
