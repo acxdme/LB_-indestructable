81203584535200fa2397122a401986118273f776
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-08-08 18:01:40 +0530

    Word-Ladder 1

--- Graph/WordLadder.cpp
+++ Graph/WordLadder.cpp
@@ -48,3 +48,58 @@ public:
         
     }
 };
+
+
+
+//------------------------------------------------------------------------------------------------------
+// be very careful about what data-structure you use
+// q => pq , vector => unordered_set
+
+class Solution {
+public:
+
+    int bfs(string &beginWord, string &endWord, vector<string>& words){
+
+        priority_queue< pair<int,string>, vector<pair<int,string>> , greater<pair<int,string>> > q; // {distance,word}
+        q.push({1,beginWord});
+        unordered_set<string> wordList(words.begin(),words.end());
+
+        while(!q.empty()){
+            auto it = q.top();
+            q.pop();
+
+            int distance = it.first;
+            string word = it.second;
+            
+            //base-edge : result found
+            if(word == endWord){
+                return distance;
+            }
+
+            for(int i =0 ; i < word.size();i++){
+                char originalChar = word[i];
+                for(char c = 'a' ; c <= 'z' ; c++){
+                    word[i] = c;
+                    auto itr = wordList.find(word);
+                    if( itr != wordList.end()){
+                        q.push({distance + 1,word});
+                        wordList.erase(itr);
+                    }
+                }
+                word[i] = originalChar;
+            }
+        }
+
+        return 0;
+    }
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        if(find(wordList.begin(),wordList.end(),endWord) == wordList.end()){
+            return 0;
+        }
+
+        int ans = bfs(beginWord, endWord , wordList);
+
+        return ans ;
+        
+    }
+};
