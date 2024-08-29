9d99e4b902a74adc068eb341fcc059a3b3642ed4
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-08-29 16:30:20 +0530

    Middle of Linked List

--- LinkedList/04_MiddleofLL.cpp
+++ LinkedList/04_MiddleofLL.cpp
@@ -20,3 +20,27 @@ public:
         
     }
 };
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+// gfg : https://www.geeksforgeeks.org/problems/finding-middle-element-in-a-linked-list/1
+// tc : O(N)
+// sc : O(1)
+
+    int getMiddle(Node* head) {
+        // base-case
+        if(head == NULL) return -1;
+        Node* slow = head;
+        Node* fast = head;
+        
+        while(fast != NULL){
+            fast = fast->next;
+            if(fast != NULL)
+            {
+                fast = fast->next;
+                slow = slow->next;
+            }
+        }
+        
+          
+       return slow->data;
+    }
